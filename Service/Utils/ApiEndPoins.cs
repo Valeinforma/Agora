ea4701668396fe61f630b9e438efeaa19a1665e0
Gonza-Acu <baltazaracuna33@gmail.com>
2025-08-21 19:04:50 -0300

    Añadidos nuevos proyectos y mejoras en la UI
    Se han realizado múltiples cambios en varios archivos del proyecto, incluyendo la adición de nuevos proyectos "WebBlazor" y "TestAgora", y la implementación de la interfaz genérica `IGenericService<T>` para operaciones CRUD asíncronas.
    
    Se han actualizado los recursos y se ha mejorado la estructura de la interfaz de usuario, incluyendo un nuevo menú de navegación y estilos CSS. También se han añadido nuevos iconos y se han actualizado las configuraciones de inicio.
    
    Además, se han incluido nuevos datos en `weather.json` que representan las condiciones climáticas para varias fechas.

---
+++ Service/Utils/ApiEndPoins.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Utils
+{
+    internal class ApiEndPoins
+    {
+        public static string Capacitacion { get; set; } = "capacitaciones";
+        public static string Usuario { get; set; } = "usuarios";
+        public static string Inscripcion { get; set; } = "Inscripciones";
+        public static string TipoInscripcion { get; set; } = "TipoInscripciones";
+        public static string TipoInscripcionCapacitacion { get; set; } = "TiposInscripcionesCapacitaciones";
+
+
+        public static string GetEndpoint(string name)
+        {
+            return name switch
+            {
+                nameof(Capacitacion) => Capacitacion,
+                nameof(Usuario) => Usuario,
+                nameof(Inscripcion) => Inscripcion,
+                nameof(TipoInscripcion) => TipoInscripcion,
+                nameof(TipoInscripcionCapacitacion) => TipoInscripcionCapacitacion,
+                _ => throw new ArgumentException($"Endpoint '{name}' no está definido.")
+            };
+        }
+    }
+}
