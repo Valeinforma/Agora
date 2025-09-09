51fec1634455a43effb1ace63f5dd0ca8aa25e3a
valeinforma <valemacha1805@gmail.com>
2025-09-09 20:48:25 -0300

    Mejoras en modelos de vista y configuración de la app
    Se han añadido directivas `using` en `AgoraShellViewModel.cs` y `SignInViewModel.cs` para incluir bibliotecas necesarias.
    Se ha implementado la clase `AgoraShellViewModel` con propiedades y comandos para manejar el cierre de sesión.
    En `SignInViewModel.cs`, se ha corregido el nombre de la clase y se han añadido propiedades observables para gestionar el registro de usuarios, incluyendo el envío de correos de verificación.
    Además, se ha actualizado la URL de `UrlApi` en `Resources.resx` y se han añadido archivos de configuración generados automáticamente para mantener la funcionalidad de configuración de la aplicación.

--- Service/Properties/Resources.Designer.cs
+++ Service/Properties/Resources.Designer.cs
@@ -79,7 +79,7 @@ namespace Service.Properties {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a https://apiagoravale.azurewebsites.net/api/.
+        ///   Busca una cadena traducida similar a https://apiagoravalen.azurewebsites.net/api/.
         /// </summary>
         public static string UrlApi {
             get {
