9e562538788a164b338dc2a04a1f7e040d042a42
valeinforma <valemacha1805@gmail.com>
2025-10-02 17:34:20 -0300

    correciones

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -31,7 +31,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigins",
         builder => builder
-            .WithOrigins("https://localhost:8000", "https://agoravale.azurewebsites.net")
+            .WithOrigins("https://localhost:8000", "https://agoravalen.azurewebsites.net")
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
