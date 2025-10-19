a0207f7dd57e8b7cadab6ec275fef3a043c71fea
valeinforma <valemacha1805@gmail.com>
2025-10-19 20:20:18 -0300

    Actualiza fechas y estructura de la base de datos
    Se han actualizado las fechas de las capacitaciones en `AgoraContextModelSnapshot.cs` para los IDs 1, 2 y 3.
    Se renombró la columna `DeleteTime` a `DeleteDate` y se cambió `TipoUsuarioEnum` a `TipoUsuario` en la tabla `Usuarios`.
    Además, se actualizó la cadena de conexión `mysqlRemote` en `appsettings.json` para un nuevo servidor de base de datos.
    Se creó la migración `CambiandoDatos` para reflejar estos cambios y se ajustaron las propiedades en `CambiandoDatos.Designer.cs` para las entidades afectadas.

---
+++ Backend/Migrations/20251019231914_CambiandoDatos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambiandoDatos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TipoUsuarioEnum",
+                table: "Usuarios",
+                newName: "TipoUsuario");
+
+            migrationBuilder.RenameColumn(
+                name: "DeleteTime",
+                table: "Usuarios",
+                newName: "DeleteDate");
+
+            migrationBuilder.UpdateData(
+                table: "Capacitaciones",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "FechaHora",
+                value: new DateTime(2025, 10, 29, 20, 19, 10, 908, DateTimeKind.Local).AddTicks(6764));
+
+            migrationBuilder.UpdateData(
+                table: "Capacitaciones",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "FechaHora",
+                value: new DateTime(2025, 11, 8, 20, 19, 10, 908, DateTimeKind.Local).AddTicks(6795));
+
+            migrationBuilder.UpdateData(
+                table: "Capacitaciones",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "FechaHora",
+                value: new DateTime(2025, 11, 8, 20, 19, 10, 908, DateTimeKind.Local).AddTicks(6799));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TipoUsuario",
+                table: "Usuarios",
+                newName: "TipoUsuarioEnum");
+
+            migrationBuilder.RenameColumn(
+                name: "DeleteDate",
+                table: "Usuarios",
+                newName: "DeleteTime");
+
+            migrationBuilder.UpdateData(
+                table: "Capacitaciones",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "FechaHora",
+                value: new DateTime(2025, 10, 19, 17, 13, 58, 550, DateTimeKind.Local).AddTicks(2842));
+
+            migrationBuilder.UpdateData(
+                table: "Capacitaciones",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "FechaHora",
+                value: new DateTime(2025, 10, 29, 17, 13, 58, 550, DateTimeKind.Local).AddTicks(2872));
+
+            migrationBuilder.UpdateData(
+                table: "Capacitaciones",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "FechaHora",
+                value: new DateTime(2025, 10, 29, 17, 13, 58, 550, DateTimeKind.Local).AddTicks(2877));
+        }
+    }
+}
